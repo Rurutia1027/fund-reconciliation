89242c56d3a5b42f07e896e77809bf19796e22ed
Rurutia1027 <rurutia1027@gmail.com>
2025-05-08 19:12:05 +0800

    add airflow py script for preparing etl executione env

---
+++ airflow/dags/init_etl_env.py
@@ -0,0 +1,156 @@
+from datetime import timedelta, datetime 
+import pytz 
+from airflow import DAG 
+from airflow.operators.postgres_operator import PostgresOperator 
+
+default_args = {
+    'owner':'airflow',
+    'depends_on_past': False, 
+    'email': ['airflow@example.com'],
+    'email_on_failure': False, 
+    'email_on_retry': False, 
+    'retries': 1, 
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG (
+    'init_etl_env',
+    default_args=default_args,
+    description='Initialize ETL Environment',
+    schedule_interval='@once',
+    start_date=datetime(2025, 1, 1, 0, 0, 0, 0, pytz.UTC),
+    tags=['init'],
+    is_paused_upon_creation=False 
+)
+
+create_schema_task = PostgresOperator(
+    task_id='create_schemas',
+    sql = """
+    Create schema if not exists import; 
+    Create schema if not exists warehouse; 
+    Create schema if not exsits ops; 
+    Create schema if not exsits staging; 
+    """,
+    dag = dag, 
+    postgres_conn_id = "sales_dw",
+    autocommit = True 
+)
+
+create_registry_table = PostgresOperator(
+    task_id='create_registry_table',
+    sql="""
+    CREATE TABLE IF NOT EXISTS ops.FlatFileLoadRegistry(
+        EntryID serial PRIMARY KEY,
+        Filename varchar(255) UNIQUE,
+        Extension varchar(10),
+        LoadDate timestamp,
+        Processed boolean, 
+        Validated boolean); 
+    """,
+    dag = dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True
+)
+
+create_csv_destination = PostgresOperator(
+    task_id = 'create_csv_destination',
+    sql = """
+    CREATE TABLE IF NOT EXISTS import.ResellerCSV (
+        Transaction_ID int, 
+        Product_name varchar(255),
+        Number_of_purchased_postcards int, 
+        Total_amount money, 
+        Sales_Channel varchar(255),
+        Customer_First_Name varchar(255),
+        Customer_Last_Name varchar(255),
+        Customer_Email varchar(255),
+        Office_Location varchar(255),
+        Created_Date date, 
+        Imported_File varchar(255),
+        Loaded_Timestamp timestamp not null default now()
+    ); 
+    """, 
+    dag = dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_xml_destination_task = PostgresOperator(
+    task_id = 'create_xml_destination',
+    sql = """
+    CREATE TABLE IF NOT EXISTS imprt.ResellerXML (
+        EntryID serial PRIMARY KEY,
+        Data jsonb, 
+        Loaded_Timestamp timestamp not null default now()
+    ); 
+    """, 
+    dag = dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_transaction_task = PostgresOperator(
+    task_id='create_transactions_destination', 
+    sql = """
+    CREATE TABLE IF NOT EXISTS import.transactions (
+        customer_id int, 
+        product_id int, 
+        amount money, 
+        qty int, 
+        channel_id int, 
+        bought_date date, 
+        transaction_id int, 
+        Loaded_Timestamp timestamp not null default now()
+    ); 
+    """,
+    dag=dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_resellers_destination_task = PostgresOperator(
+    task_id = 'create_resellers_destination',
+    sql = """""",
+    dag = dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_channels_destination_task = PostgresOperator(
+    task_id = 'create_channels_destination',
+    sql = """
+    CREATE TABLE IF NOT EXISTS imprt.channels(channel_id int, 
+        channel_name VARCHAR(255), 
+        Loaded_Timestamp timestamp not null default now()
+    ); 
+    """,
+    dag  =dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_customers_destination_task = PostgresOperator(
+    task_id = 'create_customers_destination',
+    sql = """
+    CREATE TABLE IF NOT EXISTS import.customers(customer_id int, first_name VARCHAR(255), 
+        email VARCHAR(255),
+        last_name VARCHAR(255), 
+        Loaded_Timestamp timestamp not null default now()
+        ); 
+    """,
+    dag=dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_products_destination_task = PostgresOperator(
+    task_id = 'create_products_destination',
+    sql = """
+    """, 
+    dag=dag, 
+    postgres_conn_id = 'sales_dw',
+    autocommit = True 
+)
+
+create_channels_task >> create_registry_table >> [create_csv_destination, create_xml_destination_task]
+create_schemas_task >> [create_transaction_task, create_resellers_destination_task, create_channels_destination_task, create_customers_destination_task, create_products_destination_task]
